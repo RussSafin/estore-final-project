1445b7e2c605abf7ff0293b59b85ad6f0df7c1b1
Russ Safin <russ.safin@gmail.com>
2021-12-08 03:36:14 -0800

    add Router and create first Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Home from './component/Home';
+import Products from './component/Products';
+import {Switch, Route} from 'react-router-dom';
+
 
 function App() {
     return (
         <div>
             <Navbar/>
-            <Home />
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/products" component={Products}/>
+                <Home/>
+            </Switch>
         </div>
     );
 }
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Products from './Products';
 
 const Home = () => {
     return (
         <div className="hero">
             <div className="card bg-dark text-white border-0">
-                <img src="assets/bg.jpg" className="card-img" alt="Background"/>
+                <img src="assets/bg.jpg" className="card-img" alt="Background" height="550px"/>
                 <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className="container">
                         <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
@@ -12,6 +13,7 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            <Products/>
         </div>
     );
 }
