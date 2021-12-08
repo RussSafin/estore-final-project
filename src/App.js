f1128da5f1690280daac6123f0a3bb9eebc26db3
Russ Safin <russ.safin@gmail.com>
2021-12-08 04:39:05 -0800

    Add Product component and finish Routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import Navbar from './component/Navbar';
 import Home from './component/Home';
 import Products from './component/Products';
+import Product from './component/Product';
+
 import {Switch, Route} from 'react-router-dom';
+
 
 
 function App() {
@@ -12,6 +15,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/products" component={Products}/>
+                <Route exact path="/products/:id" component={Product} />
                 <Home/>
             </Switch>
         </div>
--- src/component/Products.js
+++ src/component/Products.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {NavLink} from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
@@ -77,9 +78,9 @@ const Products = () => {
                                     <div className="card-body">
                                         <h5 className="card-title mb-0">{product.title.substring(0, 12)}...</h5>
                                         <p className="card-text lead fw-bold">${product.price}</p>
-                                        <a href="#" className="btn btn-outline-dark">
+                                        <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">
                                             Buy Now
-                                        </a>
+                                        </NavLink>
                                     </div>
                                 </div>
                             </div>
