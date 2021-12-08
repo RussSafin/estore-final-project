34e1c6ae07681faba657de60a494b87da604949a
Russ Safin <russ.safin@gmail.com>
2021-12-08 00:57:38 -0800

    add Home component and styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './component/Navbar';
+import Home from './component/Home';
 
 function App() {
     return (
         <div>
             <Navbar/>
+            <Home />
         </div>
     );
 }
